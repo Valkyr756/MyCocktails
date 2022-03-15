87162a96360c08e942ec266165434a84f7088d1f
Daniel Garin <al385773@uji.es>
2022-03-15 18:29:35 +0100

    Fourth Commits

--- app/src/main/java/uji/al385773/mycocktails/Model/Database/Network.kt
+++ app/src/main/java/uji/al385773/mycocktails/Model/Database/Network.kt
@@ -1,43 +1,59 @@
 package uji.al385773.mycocktails.Model.Database
 
 import android.content.Context
+import com.android.volley.Request
 import com.android.volley.Response
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONException
+import org.json.JSONObject
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import uji.al385773.mycocktails.Model.Database.DAO
+import javax.xml.transform.ErrorListener
 
 class Network private constructor(context: Context) {
 
-    companion object : SingletonHolder<Network, Context>(::Network) {
+    companion object : SingletonHolder<Network, Context>(::Network)
+
+    /*private val network = Network.getInstance(context)
+    private val database = Database.getInstance(context)*/
+    private val queue = Volley.newRequestQueue(context)
+    private val LIST_LABEL = "categories"
+    private val CATEGORY_NAME_LABEL = "name"
+
+    fun getCategories(listener: Response.Listener<List<Category>>, errorListener: Response.ErrorListener) {
+
+        val url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list"
+        val request = JsonObjectRequest(
+            Request.Method.GET,
+            url,
+            null,
+            { response -> processCategories(response, listener) },
+            { error -> errorListener.onErrorResponse(error) }
+        )
+        queue.add(request)
     }
 
-    fun getCategories(listener: Response.Listener<List<Category>>,
-                      errorListener: Response.ErrorListener) =
-        // Launch a coroutine
-        GlobalScope.launch(Dispatchers.Main) {
-            // Blocking call to read the categories from the DAO
-            val categories = withContext(Dispatchers.IO) {
-                database.dao.getCategories()
+    private fun processCategories(response: JSONObject, listener: Response.Listener<List<Category>>) {
+
+        val categories = ArrayList<Category>()
+        try {
+            val categoryArray = response.getJSONArray(LIST_LABEL)
+            for (i in 0 until categoryArray.length()) {
+                val categoryObject = categoryArray[i] as JSONObject
+                val name = categoryObject.getString(CATEGORY_NAME_LABEL)
+                categories.add(Category(name))
             }
-            if (categories.isEmpty()) {
-                // Recover categories from the net
-                network.getCategories(Response.Listener {
-                    // Launch a coroutine to store the
-                    // categories in the database
-                    GlobalScope.launch {
-                        database.dao.insertCategories(it)
-                    }
-                    // Pass the categories to the listener
-                    listener.onResponse(it)
-                }, Response.ErrorListener {
-                    errorListener.onErrorResponse(it)
-                })
-            }
-            else
-                // Pass the categories to the listener
-                listener.onResponse(categories)
+        } catch (e: JSONException) {
+            errorListener.onErrorResponse(VolleyError("BAD JSON FORMAT"))
+            return
         }
+        categories.sortBy { it.name }
+        listener.onResponse(categories)
+    }
 }
     
